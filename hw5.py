aa4ce1a639cde5a285533a8fd47d9ea56a68e2ed
candr006 <candr006@ucr.edu>
2018-12-07 07:19:58 +0000

    return when exact root is found

--- hw5.py
+++ hw5.py
@@ -3,25 +3,13 @@ from math import *
 
 #x^3-2x-5
 def f1(x):
-	return (x*x*x)-(2*x)-5
+	return (x*x*x)-(3*(x*x))+(3*x)-1
 
 #To change function, comment out above return expression, and either type in your own, or uncomment one of the below expressions
-	#return math.exp(-x)-x
-	#return (x*math.sin(x))-1
+	#return (x*x*x)-(2*x)-5
+	#return exp(-x)-x
+	#return (x*sin(x))-1
 	#(x*x*x)-(3*(x*x))+(3*x)-1
-
-
-#e^-x-x
-def f2(x):
-	return math.exp(-x)-x
-
-#xsin(x)-1
-def f3(x):
-	return (x*math.sin(x))-1
-
-#x^3-3x+3x-1
-def f4(x):
-	return (x*x*x)-(3*(x*x))+(3*x)-1
 
 def bisection():
 	a=0
@@ -29,7 +17,7 @@ def bisection():
 	#initialize x
 	i=1
 	#error
-	e=0.00000000001
+	e=0.001
 
 	#evaluate for f1: #x^3-2x-5
 	while 1==1:
@@ -39,12 +27,18 @@ def bisection():
 		    b+=1
 
 		f1_a=f1(a)
+		if(f1_a==0):
+			print ('Root: '+str(a))
+			return
 		f1_b=f1(b)
+		if(f1_b==0):
+			print ('Root: '+str(b))
+			return
 		i+=1
 
 		if ((f1_a*f1_b)<0):
 			#found bisection initial vals
-			print('Bisection Initial a: '+str(a)+' b: '+str(b))
+			print('Bisection Initial Values a: '+str(a)+' b: '+str(b))
 			break
 
 	while 1==1:
